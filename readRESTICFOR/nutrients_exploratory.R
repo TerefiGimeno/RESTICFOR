7789465f58b117c8a9a01d8b9625efa6ef21da45
TerefiGimeno <tegimeno@gmail.com>
2026-04-14 17:03:09 +0200

    C total from Sevilla

--- readRESTICFOR/nutrients_exploratory.R
+++ readRESTICFOR/nutrients_exploratory.R
@@ -1,6 +1,5 @@
 ####0. Load libraries####
 
-library(tidyverse)
 library(lme4)
 library(ellipse)
 library(Rmisc)
@@ -20,6 +19,7 @@ library(lmerTest)
 library(ggsignif)
 library(broom.mixed)
 library(patchwork)
+library(tidyverse)
 
 ####1. read the data from Jena####
 
@@ -59,8 +59,10 @@ summary(m_Ctotal)
 anova(m_Ctotal)
 car::Anova(m_Ctotal)
 r.squaredGLMM(m_Ctotal)
+windows(12, 8)
 check_model(m_Ctotal)
 emmeans(m_Ctotal, pairwise ~ site*type)
+emmeans(m_Ctotal, pairwise ~ site*type*depth)
 emmeans(m_Ctotal, pairwise ~ site)
 plot_model(m_Ctotal, type="pred", terms=c("site"))
 plot_model(m_Ctotal, type="pred", terms=c("type"))
@@ -104,35 +106,34 @@ plot_model(m_ratio, type="pred", terms=c("site", "type", "depth"))
 
 ####3. Read and pre-process d15N data####
 
-d15N_pre <- read.csv("dataRESTICFOR/d15N_soils.csv") %>% 
+d15N <- read.csv("dataRESTICFOR/d15N_soils.csv") %>% 
   group_by(Muestra) %>% 
   summarize(Nperc = mean(N_perc, na.rm = T),
-            d15N_permil = mean(d15N_permil_air, na.rm = T))
-
-d15N <- read.csv("dataRESTICFOR/d15N_soils.csv") %>% 
+            d15N_permil = mean(d15N_permil_air, na.rm = T)) %>% 
   separate(Muestra, into = c("site", "crap", "crap2"), sep = "-") %>%
   separate(crap2, into = c("type", "point"), sep = "_") %>% 
   mutate(plot = paste0(site, "_", type, "_", crap)) %>% 
-  select(-c(crap, weight_mg, obs)) %>%
+  select(-c(crap)) %>%
   relocate(plot) %>% 
   relocate(c(site, type))
 
 ####4. exploratory analyses for total N and d15N from U. Coruña####
-hist(d15N$N_perc)
-hist(d15N$d15N_permil_air)
+hist(d15N$Nperc)
+hist(d15N$d15N_permil)
 
 N_conc_plot <- 
-  ggplot(d15N, aes(x = site, y = N_perc, fill = type)) +
+  ggplot(d15N, aes(x = site, y = Nperc, fill = type)) +
   geom_boxplot() +
   xlab("") +
   ylab("[N] (%)")
 
-m_N_sai <- lme4::lmer(N_perc ~ site * type + (1|site:plot), data = d15N)
+m_N_sai <- lme4::lmer(Nperc ~ site * type + (1|site:plot), data = d15N)
 
 summary(m_N_sai)
 anova(m_N_sai)
 car::Anova(m_N_sai)
 r.squaredGLMM(m_N_sai)
+windows(12, 8)
 check_model(m_N_sai)
 emmeans(m_N_sai, pairwise ~ site*type)
 emmeans(m_N_sai, pairwise ~ site)
@@ -141,28 +142,53 @@ plot_model(m_N_sai, type="pred", terms=c("site"))
 plot_model(m_N_sai, type="pred", terms=c("type"))
 plot_model(m_N_sai, type="pred", terms=c("site", "type"))
 
-#check overdispersion:
-overdisp_fun(m_N_sai) #there is overdispersion when: ratio > 1
-
 d15N_plot <- 
-  ggplot(d15N, aes(x = site, y = d15N_permil_air, fill = type)) +
+  ggplot(d15N, aes(x = site, y = d15N_permil, fill = type)) +
   geom_boxplot() +
   xlab("") +
   ylab(expression(delta^15 * "N (\u2030)"))
 
-# there are some outliers:
-# Above 4 permil: SET-6B-PAST_P3 & SET-9-LONG-P4
-# below -3 permil: SET-18-LONG-p3 (only 4 samples in this plot!)
-
-m_d15N <- lme4::lmer(d15N_permil_air ~ site * type + (1|site:plot), data = d15N)
+m_d15N <- lme4::lmer(d15N_permil ~ site * type + (1|site:plot), data = d15N)
 
 summary(m_d15N)
 anova(m_d15N)
 car::Anova(m_d15N)
 r.squaredGLMM(m_d15N)
+windows(12, 8)
 check_model(m_d15N)
 emmeans(m_d15N, pairwise ~ site*type)
 emmeans(m_d15N, pairwise ~ site)
 plot_model(m_d15N, type="pred", terms=c("site"))
 plot_model(m_d15N, type="pred", terms=c("type"))
 plot_model(m_d15N, type="pred", terms=c("site", "type"))
+
+####5. Method comparison (Jena vs. IRNAS)####
+
+Ctotal <- read.csv("dataRESTICFOR/C_Total_Resticfor_IRNAS.csv")
+
+comp <- Ctotal %>% 
+  filter(purpose == "method_comparison") %>% 
+  separate(MUESTRA, into = c("plot", "type", "point", "depth"), sep = "_") %>%
+  mutate(site = str_extract(plot, "^[A-Za-z]+")) %>% 
+  mutate(crap = str_extract(plot, "\\d.*")) %>% 
+  mutate(depth = ifelse(depth == "10 cm", "0-10 cm", "10-20 cm")) %>% 
+  mutate(id = paste0(plot, "_", type, "_", point, "_", depth)) %>% 
+  mutate(plot = paste0(site, "_", type, "_", crap)) %>%
+  select(c(id, C_total_perc)) %>% 
+  rename(C_total_perc_IRNAS = C_total_perc) %>% 
+  left_join(jena, by = "id")
+ 
+summary(lm(Ctotal ~ C_total_perc_IRNAS, data = comp))
+# there is an outlier for the Jena data base, try removing
+summary(lm(Ctotal ~ C_total_perc_IRNAS, data = subset(comp, Ctotal <= 17)))
+confint(lm(Ctotal ~ C_total_perc_IRNAS, data = subset(comp, Ctotal <= 17)))
+# R2 improves (0.96), but the slope is significantly different from 1
+# that is values from IRNAS are consistently lower than from Jena
+
+C_total_comp <- 
+  ggplot(subset(comp, Ctotal <= 17), aes(x = C_total_perc_IRNAS, y = Ctotal)) +
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) +
+  xlab("[C] (%) IRNAS") +
+  ylab("[C] (%) Jena")
+
